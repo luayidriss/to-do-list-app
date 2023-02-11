4aa5fa718dc2ee02063b66557fadee5695c99bb3
luayidriss <idrissluay@gmail.com>
2023-02-11 22:16:57 +0000

    Add Remove Function, still needs to be tested alongside main function

--- run.py
+++ run.py
@@ -14,7 +14,21 @@ def add_task():
     due_date = input("Enter Due Date:")
     new_task = (task_name, due_date)
     tasks.append(new_task)
+    print("Task Added")
     return tasks
+
+def remove_task():
+    """
+    The use can remove any task by searching for its name
+    """
+    print("Input the name of the task you would like to remove")
+    task_name = input("Enter Task Name you want to remove:")
+    for i, (name,date) in enumerate(task):
+        del tasks[i]
+        print("Task Deleted")
+        break
+    else:
+        print("Task not found")
     
 updated_task = add_task()
 print(updated_task)
