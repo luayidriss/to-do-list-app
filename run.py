49f3db58daccbd0901c61e0924c09efa453d405f
luayidriss <idrissluay@gmail.com>
2023-02-11 22:01:39 +0000

    Add Task Add Function

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+tasks = []
+
+def add_task():
+    """
+    The user inputs the task they want to add and it will add it to the to do lisy
+    """
+    print("Please enter your task name and its due date")
+    print("The due date should follow this format MM-DD-YYYY")
+    print("Example:")
+    print("Task Name: Fold Laundry")
+    print("Due Date: 02-11-2023")
+
+    task_name = input("Enter Task Name:")
+    due_date = input("Enter Due Date:")
+    new_task = (task_name, due_date)
+    tasks.append(new_task)
+    return tasks
+    
+updated_task = add_task()
+print(updated_task)
+
